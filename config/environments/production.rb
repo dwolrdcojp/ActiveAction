c76d63c2224c8f6be628761929dafa4a78acb7b2
Max Schrementi <dwolrdcojp@gmail.com>
2020-07-31 23:10:09 -0500

    Add Users with Devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,9 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Devise configuration
+  config.action_mailer.default_url_options = { host: 'https://gentle-cliffs-24710.herokuapp.com/', port: 3000 }
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   resources :actionitems do
     resources :comments
   end
