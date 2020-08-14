33755734446859f5b4d37d944d50cec715b42ea8
Max Schrementi <dwolrdcojp@gmail.com>
2020-08-14 00:49:44 -0500

    Remove present validation for production runs stop time

--- app/models/production_run.rb
+++ app/models/production_run.rb
@@ -1,7 +1,7 @@
 class ProductionRun < ApplicationRecord
   has_many :production_hours
   validates :date, :line, :shift, :supervisor, :lead, 
-            :operator, :start_time, :stop_time, presence: true
+            :operator, :start_time, presence: true
   scope :filter_by_date, -> (date) { where date: date }
   scope :filter_by_line, -> (line) { where line: line }
   scope :filter_by_shift, -> (shift) { where shift: shift }
