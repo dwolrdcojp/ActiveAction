7d486ae696072068519c3933faecf6d81465659e
Max Schrementi <dwolrdcojp@gmail.com>
2020-08-29 04:40:56 -0500

    Add persistent data to filter selectiions on all models upon submit

--- app/models/production_run.rb
+++ app/models/production_run.rb
@@ -3,6 +3,7 @@ class ProductionRun < ApplicationRecord
   validates :date, :shift, :area, :line, :supervisor, :lead, 
             :operator, :start_time, presence: true
   scope :filter_by_date, -> (date) { where date: date }
+  scope :filter_by_area, -> (area) { where area: area }
   scope :filter_by_line, -> (line) { where line: line }
   scope :filter_by_shift, -> (shift) { where shift: shift }
   scope :filter_by_supervisor, -> (supervisor) { where supervisor: supervisor }
