f902099c04790e6155f5c4eedf89f93019743492
Max Schrementi <dwolrdcojp@gmail.com>
2020-08-31 20:38:33 -0500

    Change round method to truncate method in production hours array

--- app/models/production_hour.rb
+++ app/models/production_hour.rb
@@ -9,12 +9,12 @@ class ProductionHour < ApplicationRecord
        product: x.product,
        plan_throughput: x.plan_throughput,
        actual_throughput: x.actual_throughput,
-       tp_attainment: (x.actual_throughput / x.plan_throughput).round(2) * 100,
+       tp_attainment: (x.actual_throughput / x.plan_throughput).truncate(2) * 100,
        plan_labor: x.plan_labor,
        actual_labor: x.actual_labor,
-       plan_efficiency: (x.plan_throughput / x.plan_labor).round(2),
-       actual_efficiency: (x.actual_throughput / x.actual_labor).round(2),
-       eff_attainment: ((x.actual_throughput / x.actual_labor) / (x.plan_throughput / x.plan_labor)).round(2)  * 100,
+       plan_efficiency: (x.plan_throughput / x.plan_labor).truncate(2),
+       actual_efficiency: (x.actual_throughput / x.actual_labor).truncate(2),
+       eff_attainment: ((x.actual_throughput / x.actual_labor) / (x.plan_throughput / x.plan_labor)).truncate(2)  * 100,
        downtime: x.downtime,
        waste: x.waste,
        rework: x.rework,
@@ -24,22 +24,22 @@ class ProductionHour < ApplicationRecord
 
   def self.total_calculations(production_hours)
     array = [
-    plan_throughput = production_hours.sum(:plan_throughput).round(2),
-    actual_throughput = production_hours.sum(:actual_throughput).round(2),
+    plan_throughput = production_hours.sum(:plan_throughput).truncate(2),
+    actual_throughput = production_hours.sum(:actual_throughput).truncate(2),
     tp_attainment = "#{ProductionHour.new.sanitize((actual_throughput / plan_throughput)) * 100}%",
     plan_labor = production_hours.sum(:plan_labor),
-    actual_labor = production_hours.sum(:actual_labor).round(2),
+    actual_labor = production_hours.sum(:actual_labor).truncate(2),
     plan_efficiency = ProductionHour.new.sanitize((plan_throughput / plan_labor)),
     actual_effieciency = ProductionHour.new.sanitize((actual_throughput / actual_labor)),
     eff_attainment = "#{ProductionHour.new.sanitize((actual_effieciency / plan_efficiency rescue 0)) * 100}%",
-    downtime = production_hours.sum(:downtime).round(2),
-    waste = production_hours.sum(:waste).round(2),
-    rework = production_hours.sum(:rework).round(2),
-    avg_yield = "#{production_hours.average(:yield).to_f.round(2) * 100}%"]
+    downtime = production_hours.sum(:downtime).truncate(2),
+    waste = production_hours.sum(:waste).truncate(2),
+    rework = production_hours.sum(:rework).truncate(2),
+    avg_yield = "#{production_hours.average(:yield).to_f.truncate(2) * 100}%"]
   end
 
   def sanitize(number)
-    number.to_f.nan? ? 0 : number.round(2)
+    number.to_f.nan? ? 0 : number.truncate(2)
   end
 
 end
